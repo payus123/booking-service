2cb9dcc9790f0665e1745c8b15e3c08655e772aa
payus123 <payusakpovero@gmail.com>
2023-11-16 17:19:39 +0100

    added sse feature

---
+++ src/main/java/com/hostfully/bookingservice/service/BookingService.java
@@ -0,0 +1,31 @@
+package com.hostfully.bookingservice.service;
+
+import com.hostfully.bookingservice.models.SSEvent;
+import com.hostfully.bookingservice.models.dtos.request.BlockRequest;
+import com.hostfully.bookingservice.models.dtos.request.BookingRequest;
+import com.hostfully.bookingservice.models.dtos.request.BookingUpdateRequest;
+import com.hostfully.bookingservice.models.dtos.request.PageDto;
+import com.hostfully.bookingservice.models.dtos.response.BlockResponse;
+import com.hostfully.bookingservice.models.dtos.response.BookingResponse;
+
+import java.util.List;
+
+public interface BookingService {
+    BookingResponse createBooking(BookingRequest bookingRequest);
+
+    BookingResponse updateBooking(BookingUpdateRequest request);
+
+    List<BookingResponse> fetchAllBookings(PageDto pageDto);
+
+    List<BlockResponse> fetchAllBlocks(PageDto pageDto);
+
+    BlockResponse createBlock(BlockRequest blockRequest);
+
+    BookingResponse fetchBooking(String bookingId);
+
+    BlockResponse deleteBlock(String blockId);
+
+    void publishTransactionEvent(SSEvent event);
+
+
+}
